34affb81d3d58611ea3903ded7b775eb21b01b7b
Siddhesh Lakhani <140717222+sid-lakhani@users.noreply.github.com>
2024-07-04 09:53:51 +0530

    projects css

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,7 +5,7 @@ export default function Work() {
   return (
     <div className="md:col-span-4 flex-col items-center justify-between xl:mt-16">
       <h2 className="inline-block xl:hidden text-2xl mt-6">Projects</h2>
-      <div className="w-screen xl:w-auto xl:h-[600px] overflow-x-scroll xl:overflow-x-hidden xl:overflow-y-scroll py-6 xl:mt-12 custom-scrollbar">
+      <div className="w-screen xl:w-auto xl:h-[600px] overflow-x-scroll xl:overflow-x-hidden xl:overflow-y-scroll py-2 xl:mt-12 custom-scrollbar">
         <ProjectsCard />
       </div>
     </div>
